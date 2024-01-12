3ba30c4e9c8d404d109daccfc1cffa0de494071d
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-01-12 09:41:55 -0300

    Atualizando o hostname

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Geração da configuração de hardware
 
 - sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
+
+### Atualizando a configuração atual
+
+- sudo nixos-rebuild switch --flake .
--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
   outputs = { self, nixpkgs, ... }:
   {
     nixosConfigurations = {
-      taz-nixos = nixpkgs.lib.nixosSystem {
+      nixos = nixpkgs.lib.nixosSystem {
 	system = "x86_64-linux";
 	modules = [ ./configuration.nix ];
       };
