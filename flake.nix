22e59b3f12924082a31e9724347db288ae30b9cb
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-01-29 22:17:54 -0300

    Entendendo flakes

--- flake.nix
+++ flake.nix
@@ -1,19 +1,29 @@
 {
-
-  description = "My first flake!";
+  description = "Nixos config flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, ... }:
-  {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";
-	modules = [ ./configuration.nix ];
-      };
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            # inputs.home-manager.nixosModules.default
+          ];
+        };
+
     };
-  };
-
 }
