bb0ab44da4b5d87e60725bf7e7846ff760cff473
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-02-02 11:45:55 -0300

    configurando hyprland

--- home/common/optional/hyprland/hyprconf.nix
+++ home/common/optional/hyprland/hyprconf.nix
@@ -12,32 +12,94 @@
   wayland.windowManager.hyprland.settings = {
     "$mod" = "SUPER";
 
-     exec-once = [
-       "${pkgs.waybar}/bin/waybar"
-       "${pkgs.swaybg}/bin/swaybg -i ${toString ../../wallpapers/gruv_city.png}"
-     ];
-     
-     bind = [
-       "$mod, M, exit"
-       "$mod, Q, killactive"
-       "$mod, Return, exec, ${pkgs.kitty}/bin/kitty"
-       "$mod, F, exec, ${pkgs.firefox}/bin/firefox"
-     ] ++ (
-         # workspaces
-         # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-         builtins.concatLists (builtins.genList (
-             x: let
-               ws = let
-                 c = (x + 1) / 10;
-               in
-                 builtins.toString (x + 1 - (c * 10));
-             in [
-               "$mod, ${ws}, workspace, ${toString (x + 1)}"
-               "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-             ]
-           )
-           10)
-       );
+    exec-once = [
+      "${pkgs.waybar}/bin/waybar"
+      "${pkgs.swaybg}/bin/swaybg -i ${toString ../../wallpapers/gruv_city.png}"
+    ];
+
+    input = {
+      kb_layout = "br";
+      follow_mouse = 1;
+      sensitivity = 0.5;
+    };
+
+    monitor = [ "VGA-1,preferred,1680x0,1" "DP-2,preferred,0x0,1" ];
+
+    general = {
+      gaps_in = 4;
+      gaps_out = 8;
+      border_size = 2;
+      layout = "dwindle";
+      allow_tearing = false;
+      "col.active_border" = "rgba(bdae93ee) rgba(282828ee) 45deg";
+      "col.inactive_border" = "rgba(282828ee)";
+    };
+
+    decoration = {
+      rounding = 6;
+      blur = {
+        enabled = true;
+        passes = 3;
+        size = 10;
+      };
+      drop_shadow = false;
+    };
+
+    animations = {
+      enabled = true;
+
+      bezier = [ 
+        "wind, 0.05, 0.9, 0.1, 1.05"
+        "winIn, 0.1, 1.1, 0.1, 1.1"
+        "winOut, 0.3, -0.3, 0, 1"
+        "liner, 1, 1, 1, 1" 
+      ];
+
+      animation = [
+        "windows, 1, 6, wind, slide"
+        "windowsIn, 1, 6, winIn, slide"
+        "windowsOut, 1, 5, winOut, slide"
+        "windowsMove, 1, 5, wind, slide"
+        "border, 1, 1, liner"
+        "borderangle, 1, 30, liner, loop"
+        "fade, 1, 10, default"
+        "workspaces, 1, 5, wind"
+      ];
+    };
+
+    dwindle = {
+      no_gaps_when_only = false;
+      pseudotile = true; 
+      preserve_split = true; 
+      force_split = 2;
+    };
+
+    misc = {
+      vfr = true;
+      force_default_wallpaper = -1;
+    };
+
+    bind = [
+      "$mod, M, exit"
+      "$mod, Q, killactive"
+      "$mod, Return, exec, ${pkgs.kitty}/bin/kitty"
+      "$mod, F, exec, ${pkgs.firefox}/bin/firefox"
+    ] ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+        builtins.concatLists (builtins.genList (
+            x: let
+              ws = let
+                c = (x + 1) / 10;
+              in
+                builtins.toString (x + 1 - (c * 10));
+            in [
+              "$mod, ${ws}, workspace, ${toString (x + 1)}"
+              "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+            ]
+          )
+          10)
+      );
   };
 
 }
