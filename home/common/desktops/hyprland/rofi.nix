f2116ad525d3f695472b9fbe553f5783e5654455
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-02-12 15:38:33 -0300

    finish rofi

--- home/common/desktops/hyprland/rofi.nix
+++ home/common/desktops/hyprland/rofi.nix
@@ -1,79 +1,104 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
+
   programs.rofi = {
     enable = true;
-    cycle = false;
-
     package = pkgs.rofi-wayland;
 
-    extraConfig = {
-      modi = "drun,filebrowser";
-      font = "Noto Sans CJK JP 12";
-      show-icons = true;
-      disable-history = true;
-      bw = 0;
-      display-drun = "";
-      display-window = "";
-      display-combi = "";
-      icon-theme = "Fluent-dark";
-      terminal = "kitty";
-      drun-match-fields = "name";
-      drun-display-format = "{name}";
-      me-select-entry = "";
-      me-accept-entry = "MousePrimary";
-    };
-
     theme = let inherit (config.lib.formats.rasi) mkLiteral; in {
+
+      "configuration" = {
+        modi = "drun";
+        font = "Rubik 10";
+        show-icons = true;
+        icons-theme = "Reversal-dark";
+        display-drun = "";
+        drun-display-format = "{name}";
+        sidebar-mode = false;
+      };
+
       "*" = {
-        font = "Noto Sans CJK JP Bold 12";
-        background-color = mkLiteral "#${config.colorScheme.palette.base00}60";
-        foreground = mkLiteral "#${config.colorScheme.palette.base00}60";
-        text-color = mkLiteral "#${config.colorScheme.palette.base05}";
+        background = mkLiteral "#${config.colorScheme.palette.base00}";
+        foreground = mkLiteral "#${config.colorScheme.palette.base06}";
+        selected = mkLiteral "#${config.colorScheme.palette.base01}";
+
+        background-color = mkLiteral "var(background)";
+        text-color = mkLiteral "var(foreground)";
       };
 
-      window = {
-        border= mkLiteral "3px 3px 2px 3px";
-        border-color= mkLiteral "#${config.colorScheme.palette.base0D}";
-        border-radius= mkLiteral "6px 6px 0px 0px";
-        location = mkLiteral "center";
-        spacing = mkLiteral "2px";
-        children = map mkLiteral [ "mainbox" ];
+      "window" = {
+        border-radius = mkLiteral "7px";
+        width = mkLiteral "50%";
+        padding = mkLiteral "32px";
       };
 
-      mainbox = {
-        spacing = mkLiteral "0";
-        children = map mkLiteral [ "inputbar" "message" "listview" ];
+      "prompt" = {
+        background-color = mkLiteral "var(selected)";
+        enabled = true;
+        padding = mkLiteral "0.5% 32px 0% -0.5%";
+        font = "Rubik 10";
       };
 
-      inputbar = {
-        padding= mkLiteral "11px";
-        border= mkLiteral "0px 0px 2px 0px";
-        border-color= mkLiteral "#${config.colorScheme.palette.base0D}";
-        border-radius= mkLiteral "6px 6px 0px 0px";
+      "entry" = {
+        placeholder = "Search";
+        background-color = mkLiteral "var(selected)";
+        placeholder-color = mkLiteral "var(foreground)";
+        expand = true;
+        padding = mkLiteral "0.15% 0% 0% 0%";
       };
 
-      message = {
-        padding = mkLiteral "0";
-        border-color = mkLiteral "#${config.colorScheme.palette.base0D}";
-        border = mkLiteral "0px 1px 1px 1px";
+      "mainbox" = { 
+        children = [ "inputbar" "listview" ]; 
+        spacing = mkLiteral "2%";
+        padding = mkLiteral "2% 1% 2% 1%";
       };
 
-      lastview = {
-        border = mkLiteral "0px 3px 3px 3px"; 
-        border-radius = mkLiteral "0px 0px 6px 6px";
-        border-color = mkLiteral "#${config.colorScheme.palette.base0D}";
+      "inputbar" = {
+        children = [ "prompt" "entry" ];
+        background-color = mkLiteral "var(selected)";
+        expand = false;
+        border-radius = mkLiteral "6px";
+        margin = mkLiteral "0%";
+        padding = mkLiteral "10px";
       };
 
-      button = {
-        border = mkLiteral "2px 0px 2px 2px";
-        border-radius = mkLiteral "4px 0px 0px 4px";
-        border-color = mkLiteral "#${config.colorScheme.palette.base0D}";
+      "listview" = {
+      	columns = mkLiteral "4";
+      	lines = mkLiteral "3";
+      	cycle = false;
+      	dynamic = true;
+      	layout = mkLiteral "vertical";
+      };
+      
+      "element" = {
+      	orientation = mkLiteral "vertical";
+      	padding = mkLiteral "2% 0% 2% 0%";
+      };
+      
+      "element-icon" = {
+      	size = mkLiteral "48px";
+      	horizontal-align = mkLiteral "0.5";
+      };
+      
+      "element-text" = {
+      	expand = true;
+      	vertical-align = mkLiteral "0.5";
+      	horizontal-align = mkLiteral "0.5";
+      	margin = "0.5% 0.5% -0.5% 0.5%";
       };
 
-      "element selected.normal" = {
-        background-color = mkLiteral "#${config.colorScheme.palette.base03}";
+      "element-text, element-icon" = {
+      	background-color = mkLiteral "inherit";
+      	text-color = mkLiteral "inherit";
       };
+
+      "element selected" = {
+      	background-color = mkLiteral "var(selected)";
+      	border-radius = mkLiteral "6px";
+      };
+
     };
   };
+
 }
