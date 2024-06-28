f8a00807972d1eb31bad414c91c9aed2c05f6ad3
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2024-06-28 09:43:38 -0300

    chore: fmt files

--- profiles/general/default.nix
+++ profiles/general/default.nix
@@ -18,8 +18,14 @@
   localtime.locale = systemSettings.locale;
 
   # --- Hardware --- #
-  uefi.enable = if (systemSettings.bootmode == "uefi") then true else false; 
-  grub.enable = if (systemSettings.bootmode == "grub") then true else false; 
+  uefi.enable =
+    if (systemSettings.bootmode == "uefi")
+    then true
+    else false;
+  grub.enable =
+    if (systemSettings.bootmode == "grub")
+    then true
+    else false;
   grub.device = systemSettings.grubDevice;
 
   environment.systemPackages = with pkgs; [
--- templates/poetry/flake.nix
+++ templates/poetry/flake.nix
@@ -1,37 +1,45 @@
 {
-  description = "Application packaged using poetry2nix";
+  description = "Python Language Template";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
-      in
-      {
+        inherit
+          (
+            poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}
+          )
+          mkPoetryApplication
+          ;
+      in {
         packages = {
-          myapp = mkPoetryApplication { 
+          myapp = mkPoetryApplication {
             projectDir = ./.;
             preferWheels = true;
-            propagatedBuildInputs = with pkgs; []; 
+            propagatedBuildInputs = with pkgs; [];
           };
           default = self.packages.${system}.myapp;
         };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = [ self.packages.${system}.myapp ];
-          packages = [ pkgs.poetry ];
+          inputsFrom = [self.packages.${system}.myapp];
+          packages = [pkgs.poetry];
         };
-      });
+      }
+    );
 }
